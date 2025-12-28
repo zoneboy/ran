737181c17b6c9cc3d43929d59af7292d43186b95
Taofeek Lateef <36007365+zoneboy@users.noreply.github.com>
2025-12-28 09:06:56 +0100

    Delete pages directory

--- pages/services/public/services/mockData.ts
+++
@@ -1,6 +0,0 @@
-// This file is deprecated. The application now connects to a live backend.
-// Do not use these mock exports.
-
-export const MOCK_USERS = [];
-export const MOCK_ANNOUNCEMENTS = [];
-export const MOCK_PAYMENTS = [];
