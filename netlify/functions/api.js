54940924537bd9f529f3e6730494764b38eadb5f
Taofeek Lateef <lateef4ril32@gmail.com>
2026-01-24 15:47:02 +0100

    del

--- netlify/functions/api.js
+++
@@ -1,4 +0,0 @@
-const serverless = require('serverless-http');
-const app = require('../../backend/server');
-
-module.exports.handler = serverless(app);
--- services/mockData.ts
+++
@@ -1,4 +0,0 @@
-
-// This file is deprecated. All data is fetched from the live backend.
-// Do not use this file for any data operations.
-export {};
